9ee7d8b8801b4f80f1649b412c0dd9ed559ca637
sewkani-manya <sewkani.manya564@gmail.com>
2024-12-06 09:46:11 +0530

    added async function to debug

--- Weather/index.js
+++ Weather/index.js
@@ -1,16 +1,18 @@
-const url = 'https://weather-api-algobook.p.rapidapi.com/forecast/london';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'e090d9a01emshc965f379124497cp1aba9ejsnad7aa966465e',
-		'x-rapidapi-host': 'weather-api-algobook.p.rapidapi.com'
-	}
-};
+(async function fetchWeather() {
+	const url = 'https://foreca-weather.p.rapidapi.com/forecast/15minutely/102643743?alt=0&tempunit=C&windunit=MS&tz=Europe%2FLondon&periods=8&dataset=full';
+	const options = {
+		method: 'GET',
+		headers: {
+			'x-rapidapi-key': 'e090d9a01emshc965f379124497cp1aba9ejsnad7aa966465e',
+			'x-rapidapi-host': 'foreca-weather.p.rapidapi.com'
+		}
+	};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+	try {
+		const response = await fetch(url, options);
+		const result = await response.text();
+		console.log(result);
+	} catch (error) {
+		console.error(error);
+	}
+})();
